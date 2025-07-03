343aeb8993dbaef157f31196755692a94836c169
spko <spko0321@gmail.com>
2025-07-03 17:04:48 +0900

    refactor : TransactionStatRequest 유효성 검사 추가

--- src/main/java/com/example/mockbank/application/dto/TransactionStatRequest.java
+++ src/main/java/com/example/mockbank/application/dto/TransactionStatRequest.java
@@ -1,5 +1,6 @@
 package com.example.mockbank.application.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,9 @@ import java.time.YearMonth;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransactionStatRequest {
+    @NotNull(message = "시작 월은 필수입니다.")
     private YearMonth startYM; // ex: 2025-06
+
+    @NotNull(message = "종료 월은 필수입니다.")
     private YearMonth endYM;   // ex: 2025-07
 }
